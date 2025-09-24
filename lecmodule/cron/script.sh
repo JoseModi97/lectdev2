a4017fb1e7fc869157657c2ea2c3898ecfbd556f
Modi97 <odhisjoseph82@gmail.com>
2025-09-24 13:03:35 +0300

    Deleted lecmodule

--- lecmodule/cron/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# consolidate marks
-#/usr/bin/php /var/www/html/lecmodule/yii consolidate/course-work;
-
-#/usr/bin/php /var/www/html/lecmodule/yii consolidate/exam;
-
-# publish marks
-#/usr/bin/php /var/www/html/lecmodule/yii publish;
